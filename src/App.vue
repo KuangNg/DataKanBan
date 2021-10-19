e861cfb13fb79df790d8715f3a863941deade81e
kuangng <15775023443@163.com>
2021-10-19 09:13:24 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    
+    <!-- Map Start -->
+    <b-map class="bm-view" :center="center" :zoom="zoom"></b-map>
+    <!-- Map Ended -->
+    
+      <Row style="background:#eee;padding:20px">
+        <Col span="11">
+            <Card :bordered="false">
+                <p slot="title">无边框卡片</p>
+                <p>卡片内容</p>
+                <p>卡片内容</p>
+                <p>卡片内容</p>
+            </Card>
+        </Col>
+        <Col span="11" offset="2">
+            <Card shadow>
+                <p slot="title">使用阴影效果的卡片</p>
+                <p>卡片内容</p>
+                <p>卡片内容</p>
+                <p>卡片内容</p>
+            </Card>
+        </Col>
+    </Row>
+  </div>
+</template>
+
+<script>
+import BMap from "./components/BaiduMap.vue";
+export default {
+  name: "App",
+  components: {
+    BMap,
+  },
+  data() {
+    return {
+      center: {
+        lng: 116.404,
+        lat: 39.915,
+      },
+      zoom: 13,
+    };
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang ="less" scoped>
+.container {
+  width: 100%;
+  height: 100%;
+}
+/* Map Style Start */
+.bm {
+  &-view {
+    width: 100%;
+    height: 600px;
+  }
+}
+/* Map Style Ended */
+</style>
---
+++ src/components/BaiduMap.vue
@@ -0,0 +1,79 @@
+<template>
+  <baidu-map
+    id="mapContent"
+    class="map"
+    ak="H6EfbaNfADmMYNwp5cK9Og3Lsj13PWew"
+    :center="center"
+    :zoom="zoom"
+    @ready="mapReady"
+  >
+    <bm-marker :position="{ lng: 116.404, lat: 39.915 }" :dragging="true">
+    </bm-marker>
+  </baidu-map>
+</template>
+<script>
+import BaiduMap from "vue-baidu-map/components/map/Map.vue";
+import BmMarker from "vue-baidu-map/components/overlays/Marker.vue";//导入地图标志点
+export default {
+  name: "BMap",
+  props: {
+    /**
+     * 密钥
+     */
+    ak: {
+      type: String,
+      require: true,
+    },
+    /**
+     * 中心点
+     */
+    center: {
+      type: Object,
+      default: () => {},
+    },
+    /**
+     * 缩放
+     */
+    zoom: {
+      type: Number,
+      defualt: 15,
+    },
+    /**
+     * 覆盖点数据
+     */
+    markerData: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+
+    };
+  },
+  components: {
+    BaiduMap,
+    BmMarker,
+  },
+
+  methods: {
+    /**
+     * 地图画后设置地图默认的信息
+     * @method 地图已加载
+     * @param BMap：百度地址构件函数
+     * @param map：当前百度地图实例
+     * @param mapv：mapv构造函数
+     */
+    mapReady({ BMap, map }) {
+      console.log("地图初始准备", BMap, map);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+    .map{
+        width: 100%;
+        height: 100%;
+    }
+</style>
